7ecdcfed6ae58f12a53590d59bb083e5bb987aa7
scoopsoup <rnrdud46@nate.com>
2023-04-12 21:30:51 +0900

    feat : 두 직선의 교점 만들기

--- programmers/src/main/java/Num87377.java
+++ programmers/src/main/java/Num87377.java
@@ -10,6 +10,20 @@ public class Num87377 {// 87377_교점에 별 만들기
         }
     }
 
+    // 두 직선의 교점 만들기
+    // a1x + b1y + c1 = 0
+    // a2x + b2y + c2 = 0
+    // 두 직선의 교점 : x = ()/() , y = ()/()
+    private Point intersection(long a1, long b1, long c1, long a2, long b2, long c2) {
+        double x = (double) (b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1);
+        double y = (double) (a2 * c1 - a1 * c2) / (a1 * b2 - a2 * b1);
+
+        if (x % 1 != 0 || y % 1 != 0) {
+            return null;
+        }
+
+        return new Point((long) x, (long) y);
+    }
 
     public static void main(String args[]) {
 
