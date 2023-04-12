264a8ca775a9e717c8f5584fd59e662165dd2ea6
scoopsoup <rnrdud46@nate.com>
2023-04-12 21:42:24 +0900

    feat : 교점의 최대, 최소 구하기

--- programmers/src/main/java/Num87377.java
+++ programmers/src/main/java/Num87377.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Num87377 {// 87377_교점에 별 만들기
 
     // 좌표만들기
@@ -23,6 +25,32 @@ public class Num87377 {// 87377_교점에 별 만들기
         }
 
         return new Point((long) x, (long) y);
+    }
+
+    // 교점의 최대, 최소 구하기
+    // 교점을 찍기위한 최소한의 크기를 나타내기 위함
+    private Point getMinimumPoint(List<Point> points) {
+        Long x = Long.MAX_VALUE;
+        Long y = Long.MAX_VALUE;
+
+        for (Point point : points) {
+            x = point.x < x ? point.x : x;
+            y = point.y < y ? point.y : y;
+        }
+
+        return new Point(x, y);
+    }
+
+    private Point getMaximumPoint(List<Point> points) {
+        Long x = Long.MIN_VALUE;
+        Long y = Long.MIN_VALUE;
+
+        for (Point point : points) {
+            x = (point.x > x) ? point.x : x;
+            y = (point.y > y) ? point.y : y;
+        }
+
+        return new Point(x, y);
     }
 
     public static void main(String args[]) {
