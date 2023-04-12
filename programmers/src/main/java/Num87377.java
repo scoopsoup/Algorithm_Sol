f23cc23be12f80ae59788590f6ad0117a5db33f7
scoopsoup <rnrdud46@nate.com>
2023-04-12 21:21:24 +0900

    feat : 좌표만들기

---
+++ programmers/src/main/java/Num87377.java
@@ -0,0 +1,20 @@
+public class Num87377 {// 87377_교점에 별 만들기
+
+    // 좌표만들기
+    private static class Point {
+        public final long x, y;
+
+        private Point(long x, long y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+
+    public static void main(String args[]) {
+
+    }
+
+}
+
+
