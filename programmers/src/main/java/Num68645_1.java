40957158e968e2dd9a0fb3cfeccb123e61da20f8
scoopsoup <rnrdud46@nate.com>
2023-04-13 21:37:54 +0900

    feat : 삼각형을 배열로 만들 준비하기

---
+++ programmers/src/main/java/Num68645_1.java
@@ -0,0 +1,18 @@
+public class Num68645_1 { // 68645_삼각달팽이_sol1(이중반복문이용)
+
+    public int[] solution_68645_1(int n) {
+        // 삼각형을 이차원배열에 만들어준다.
+        // 이차원배열 생성
+        int[][] triangle = new int[n][n];
+        // 증가시킬 숫자(삼각형에 넣어줄 숫자)
+        int temp = 1;
+        // 현재 삼각형(배열)의 위치
+        // 제일 처음 위치는 (0,0)으로 설정
+        int x, y = 0;
+
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
